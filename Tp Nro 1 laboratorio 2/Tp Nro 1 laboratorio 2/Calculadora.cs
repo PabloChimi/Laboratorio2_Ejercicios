0cb42816f106caadcc49159d4130ba4fca0291ee
PabloChimi <Pablo.chimi@hotmail.com>
2018-04-16 17:13:11 -0300

    Subo tp 1 base

---
+++ Tp Nro 1 laboratorio 2/Tp Nro 1 laboratorio 2/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_Nro_1_laboratorio_2
+{
+    class Calculadora
+    {
+    }
+}
---
+++ Tp Nro 1 laboratorio 2/Tp Nro 1 laboratorio 2/Numero.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_Nro_1_laboratorio_2
+{
+    class Numero
+    {
+        private double numero;
+        
+        public Numero()
+        {
+
+        }
+
+        public Numero(string numero)
+        {
+
+        }
+
+        public Numero(double numero)
+        {
+
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            int i;
+            double valorTotal = 0;
+
+            for (i=0; i < binario.Length; i++)
+            {
+                if(char.Equals(binario[i],'1') )
+                {
+                    valorTotal = +Math.Pow(2, binario.Length - i);
+                }
+                else
+                {
+                    if(!char.Equals(binario[i], '0'))
+                    {
+                        return "Valor invalido";
+                    }
+                }
+            }
+            return valorTotal.ToString();
+
+        }
+
+
+    }
+}
